e7b10fc6d4d5d39ff0b3607d586a9f8510480e31
krzgem <krzysztof.gemser@gmail.com>
2012-11-25 16:40:30 +0100

    GUI - local icons,

---
+++ MultimediaPlayer/src/main/java/org/projekt/multimediaplayer/gui/TestMultimediaPlayerWindow.java
@@ -0,0 +1,18 @@
+package org.projekt.multimediaplayer.gui;
+
+import javax.swing.JFrame;
+
+public class TestMultimediaPlayerWindow
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		JFrame frame = new MultimediaPlayerJFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
